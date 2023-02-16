a161565b1e200ae1a7ca5092237f422d6eccf120
Prof-Percival <percival.mrd921@yahoo.com>
2023-02-16 15:51:56 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 /**
 * main - Entry point
 *
-i* Return: Always 0 (Success)
- */
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
