e1a5c2c464efb08260dacd65c1508ef0de501584
Prof-Percival <percival.mrd921@yahoo.com>
2023-02-17 13:48:52 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,9 @@
  */
 int main(void)
 {
-  char low;
-
-  for (low = 'a'; low <= 'z'; low++)
-    putchar(low);
-  printf("\n");
-  return (0);
+char low;
+for (low = 'a'; low <= 'z'; low++)
+  putchar(low);
+printf("\n");
+return (0);
 }
