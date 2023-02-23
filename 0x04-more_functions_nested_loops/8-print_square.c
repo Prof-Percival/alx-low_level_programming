037f162abfbf0238f06a10f7cdef0212e2256b1f
Prof. Percival <110649262+Prof-Percival@users.noreply.github.com>
2023-02-23 22:49:40 +0200

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,20 +8,14 @@
  * Return: Always 0 (Success)
 */
 
-void print_squares(int size)
+void print_square(int size)
 {
-int i;
-int k;
+	int row, column;
 
-if (size > 0)
-{
-for (i = 0; i < size; i++)
-{
-for (k = 0; k < size; k++)
-_putchar(35);
-_putchar('\n');
-}
-}
-else
-_putchar('\n');
+	for (row = 1; row <= size; ++row)
+	{
+		for (column = 1; column <= size; ++column)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
