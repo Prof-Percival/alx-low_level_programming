e1594bc0cba14704dc1e6c835b98950347b56925
Prof-Percival <percival.mrd921@yahoo.com>
2023-02-23 12:40:03 -0800

    Func fixed

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,14 +8,22 @@
  * Return: Always 0 (Success)
 */
 
-void print_square(int size)
+void print_squares(int size)
 {
-	int row, column;
 
-	for (row = 1; row <= size; ++row)
-	{
-		for (column = 1; column <= size; ++column)
-			_putchar('#');
-		_putchar('\n');
+  int i;
+  int k;
+
+  if (size > 0)
+    {
+      for (i = 0; i < size; i++){
+	for (k = 0; k < size; k++){
+	 _putchar(35);
 	}
+	_putchar('\n');
+      }
+    }
+  else{
+    _putchar('\n');
+  }
 }
