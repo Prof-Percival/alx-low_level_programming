3bf58c320e5024ac4cbf2eb137dc428d8e1959d5
Prof-Percival <percival.mrd921@yahoo.com>
2023-03-16 15:38:10 -0700

    Compiled

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -1,4 +1,4 @@
-c#include "main.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 #include "main.h"
 /**
  * malloc_checked - Entry Point
